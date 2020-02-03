efcd51df4b979e7e9a712f73290dabfcea39fce0
Aziz Faozi <admin@labseni.com>
2020-02-03 09:47:55 +0700

    ICR

---
+++ ocr_nanda.py
@@ -0,0 +1,40 @@
+# Author : Aziz Amerul Faozi
+# Description : this code use for testing ocr 
+#
+
+#import mysql.connector
+
+import requests
+# Declare CLass
+class POC_OCR:
+    #
+    #  POC_OCR
+    #
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def getImage():
+        # get image form database
+        #
+        print "mantap"
+        
+        
+    def TranslateOCR(self):
+        # inputan berupa gambar dari database
+        # output berupa teks yang akan masuk ke 
+        # table teks
+
+        # get post to orc api
+        # URL API
+
+        
+        
+        print "Translate OCR"
+     
+    def WriteDataBase(self):
+        # ini akan mencoba untuk menulis ke database
+        print "Print ke database"
+        
+        
+    
